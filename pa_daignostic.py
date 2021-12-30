d13164ea627f0b35f0737896dcbf9de49a41f4da
tpowell <tpowell@gbuapcd.org>
2021-12-30 09:32:40 -0800

    added gas sensors to output data file. Need to make this script smart enough to know what sensors are present and adjust output accordingly

--- pa_daignostic.py
+++ pa_daignostic.py
@@ -85,10 +85,11 @@ def print_data(tca, wait_time, n_tests, avg):
     purple_air = adafruit_ina219.INA219(tca[0])
     fans = adafruit_ina219.INA219(tca[2])
     comms  = adafruit_ina219.INA219(tca[3])
-    wifi = adafruit_ina219.INA219(tca[4])
-    raspberry_pi = adafruit_ina219.INA219(tca[7])
+    raspberry_pi = adafruit_ina219.INA219(tca[4])
+    wifi = adafruit_ina219.INA219(tca[7])
     
-    bme = adafruit_bme.Adafruit_BME680_I2C(tca[5])
+
+    bme = adafruit_bme680.Adafruit_BME680_I2C(tca[5])
     scd = adafruit_scd30.SCD30(tca[5])
     pm25 = PM25_I2C(tca[5])
     mcp = adafruit_mcp9808.MCP9808(tca[6])
@@ -102,7 +103,7 @@ def print_data(tca, wait_time, n_tests, avg):
 
     while j != n_tests:
         
-        i=0
+        i=1
         start_time = datetime.now().strftime('%m:%d:%Y %H:%M:%S')
         
         # initialize variables for averaging
@@ -176,9 +177,9 @@ def print_data(tca, wait_time, n_tests, avg):
 
             fans_current += fans.current
             fans_power += fans.power
-            fans_voltage += fans.voltage
+            fans_voltage += fans.bus_voltage
 
-            mcp_temp += temp.temperature
+            mcp_temp += mcp.temperature
 
             bme_gas += bme.gas
             bme_hum += bme.humidity
@@ -196,6 +197,8 @@ def print_data(tca, wait_time, n_tests, avg):
                 pm10_st += pmdata["pm100 standard"]
                 
                 pm_avg += 1
+            except:
+                Exception
 
             try:
                 if scd.data_available == 1:
@@ -203,7 +206,9 @@ def print_data(tca, wait_time, n_tests, avg):
                     scd_hum += scd.relative_humidity
                     scd_temp += scd.temperature
                     scd_avg += 1
-            
+            except:
+                Exception
+
             time.sleep(wait_time)
             
             i += 1
@@ -251,8 +256,8 @@ def print_data(tca, wait_time, n_tests, avg):
         
         print('Average Time: {} seconds'.format(avg), 'Average PM: {}'.format(pm_avg), 'Average SCD: {}'.format(scd_avg))
         print('Start: {}\nEnd: {}'.format(start_time, end_time))
-
-        print('Time: {:>34}\nPA Current: {:>15.2f} mA  PA Power: {:>15.2f} W  PA Voltage: {:>14.2f} V\nWiFi Current: {:>12.2f} mA  WiFi Power: {:>13.2f} W  WiFi Voltage: {:>12.2f} V\nRP Current: {:>15.2f} mA  RP Power: {:>15.2f} W  RP Voltage: {:>14.2f} V\nComms Current: {:>11.2f} mA  Comms Power: {:>12.2f} W  Comms Voltage: {:>10.2f} V\nFans Current: {:>11.2f} mA  Fans Power: {:>12.2f} W  Fans Voltage: {:>11.2f} V\nMCP Temp: {:>16.2f} C  BME Temp: {:>16.2f} C  SCD Temp: {:>16.2f} C\nBME Humidity: {:>12.2f} %  SCD Humidity: {:>12.2f}\nBME Gas: {:>17.2f} Ohms  SCD CO2: {:>17.2f} PPM\nPM1.0 Env: {:>16.2f} ug/m3  PM2.5 Env: {:>16.2f} ug/m3  PM10.0 Env: {:>15.2f} ug/m3\nPM1.0 ST: {:>17.2f} ug/m3  PM2.5 ST: {:>17.2f} ug/m3  PM10.0 ST: {:>16.2f} ug/m3'.format(start_time, avg_purple_air_current, avg_purple_air_power, avg_purple_air_voltage, avg_wifi_current, avg_wifi_power, avg_wifi_voltage, avg_raspberry_pi_current, avg_raspberry_pi_power, avg_raspberry_pi_voltage, avg_comms_current, avg_comms_power, avg_comms_voltage, avg_fans_current, avg_fans_power, avg_fans_voltage, avg_mcp_temp, avg_bme_temp, avg_scd_temp, avg_bme_hum, avg_scd_hum, avg_bme_gas, avg_scd_co2, avg_pm1_env, avg_pm25_env, avg_pm10_env, avg_pm1_st, avg_pm25_st, avg_pm10_st))
+        # terrible formatting
+        print('Time: {:>34}\nPA Current: {:>10.2f} mA      PA Power: {:>8.2f} W      PA Voltage: {:>8.2f} V\nWiFi Current: {:>9.2f} mA     WiFi Power: {:>6.2f} W      WiFi Voltage: {:>6.2f} V\nRP Current: {:>11.2f} mA      RP Power: {:>8.2f} W     RP Voltage: {:>8.2f} V\nComms Current: {:>8.2f} mA     Comms Power: {:>5.2f} W      Comms Voltage: {:>5.2f} V\nFans Current: {:>9.2f} mA     Fans Power: {:>6.2f} W      Fans Voltage: {:>6.2f} V\nMCP Temp: {:>12.2f} C       BME Temp: {:>9.2f} C     SCD Temp: {:>10.2f} C\nBME Humidity: {:>8.2f} %       SCD Humidity: {:>5.2f}\nBME Gas: {:>15.2f} Ohms  SCD CO2: {:>11.2f} PPM\nPM1.0 Env: {:>10.2f} ug/m3    PM2.5 Env: {:>7.2f} ug/m3  PM10.0 Env: {:>8.2f} ug/m3\nPM1.0 ST: {:>11.2f} ug/m3    PM2.5 ST: {:>8.2f} ug/m3  PM10.0 ST: {:>9.2f} ug/m3'.format(start_time, avg_purple_air_current, avg_purple_air_power, avg_purple_air_voltage, avg_wifi_current, avg_wifi_power, avg_wifi_voltage, avg_raspberry_pi_current, avg_raspberry_pi_power, avg_raspberry_pi_voltage, avg_comms_current, avg_comms_power, avg_comms_voltage, avg_fans_current, avg_fans_power, avg_fans_voltage, avg_mcp_temp, avg_bme_temp, avg_scd_temp, avg_bme_hum, avg_scd_hum, avg_bme_gas, avg_scd_co2, avg_pm1_env, avg_pm25_env, avg_pm10_env, avg_pm1_st, avg_pm25_st, avg_pm10_st))
 
         print()
               
@@ -263,26 +268,36 @@ def file_write(tca, wait_time, n_points, n_tests, avg):
     '''
     Write the data from the IN219s to file for export.
 
-    input param: pa_channel, list of channels to enable (default = all)
+    input param: tca, the object needed to call the individual channels the sensors are attached to on the mux board
     input param: wait_time, capture data points every wait_time seconds
     input param: n_points, number of data points to capture
     input param: n_test, number of separate tests for data capture
-    input param: data_dict, dictionary containing all of the arrays needed to store data from I2C devices
+    input param: avg, the number of points to average over. This is effectively n_points, but it's possible for it to be different depending on what the user wants.
     '''
 
     j = 0
     
     data_dir = os.getcwd() + "/data/"
-
+    
+    #INA219 devices
     purple_air = adafruit_ina219.INA219(tca[0])
-    raspberry_pi = adafruit_ina219.INA219(tca[7])
-    wifi = adafruit_ina219.INA219(tca[4])
+    fans = adafruit_ina219.INA219(tca[2])
     comms  = adafruit_ina219.INA219(tca[3])
-    temp = adafruit_mcp9808.MCP9808(tca[6])
+    raspberry_pi = adafruit_ina219.INA219(tca[4])
+    wifi = adafruit_ina219.INA219(tca[7])
+    
+
+    bme = adafruit_bme680.Adafruit_BME680_I2C(tca[5])
+    scd = adafruit_scd30.SCD30(tca[5])
+    pm25 = PM25_I2C(tca[5])
+    mcp = adafruit_mcp9808.MCP9808(tca[6])
     
     # file_name = "test_" +  datetime.now()strftime("%m%d%Y%H%M%S") + ".csv"
     header_vals = ['Time', 'PA Current', 'PA Power', 'PA Voltage', 'WIFI Current', 'WIFI Power', 'WIFI Voltage',
-                    'RPi Current', 'RPi Power', 'RPi Voltage', 'Comms Current', 'Comms Power', 'Comms Voltage', 'Temp']
+                    'RPi Current', 'RPi Power', 'RPi Voltage', 'Comms Current', 'Comms Power', 'Comms Voltage', 
+                    'Fans Current', 'Fans Power', 'Fans Voltage', 'Enclosure Temp', 'BME Temp', 'SCD Temp',
+                    'BME Humidity', 'CO2 Humidity', 'BME Gas', 'CO2 Gas', 'BME Pressure', 'PM1.0 Env',
+                    'PM2.5 Env', 'PM10.0 Env', 'PM1.0 ST', 'PM2.5 ST', 'PM10.0 ST']
 
     #while j < n_tests:
     # we don't care about the number of tests anymore, this runs continuously and breaks tests into 1 hour chunks.
@@ -308,7 +323,7 @@ def file_write(tca, wait_time, n_points, n_tests, avg):
         
             file_writer = csv.writer(f, delimiter = ',')
                 
-            i=0
+            i=1
             start_time = datetime.now().strftime('%m:%d:%Y %H:%M:%S')
             purple_air_current = 0
             purple_air_power = 0
@@ -325,8 +340,38 @@ def file_write(tca, wait_time, n_points, n_tests, avg):
             comms_current = 0
             comms_power = 0
             comms_voltage = 0
+        
+            fans_current = 0
+            fans_power = 0
+            fans_voltage = 0
 
-            temp_temp = 0
+            mcp_temp = 0
+
+            bme_gas = 0
+            bme_hum = 0
+            bme_press = 0
+            bme_temp = 0
+        
+            scd_co2 = 0
+            scd_hum = 0
+            scd_temp = 0
+        
+            # environmental conditions
+            pm1_env = 0
+            pm25_env = 0
+            pm10_env = 0
+
+            # standard pressure and temp conditions
+            pm1_st = 0
+            pm25_st = 0
+            pm10_st = 0
+
+            # because of the clock stretching on these two devices, it is possible for the data to not be present
+            # after the wait_time, so we don't record data when that happens. This means we cannot use the input average
+            # parameter for averaging.
+            # THIS NEEDS TO BE CORRECTED WITH NUMPY
+            scd_avg = 0
+            pm_avg = 0
 
             while i <= avg:
 
@@ -335,19 +380,55 @@ def file_write(tca, wait_time, n_points, n_tests, avg):
                 purple_air_current += purple_air.current
                 purple_air_power += purple_air.power
                 purple_air_voltage += purple_air.bus_voltage
+            
                 wifi_current += wifi.current
                 wifi_power += wifi.power
                 wifi_voltage += wifi.bus_voltage
+            
                 raspberry_pi_current += raspberry_pi.current
                 raspberry_pi_power += raspberry_pi.power
                 raspberry_pi_voltage += raspberry_pi.bus_voltage
+            
                 comms_current += comms.current
                 comms_power += comms.power
                 comms_voltage += comms.bus_voltage
-                temp_temp += temp.temperature
+
+                fans_current += fans.current
+                fans_power += fans.power
+                fans_voltage += fans.bus_voltage
+
+                mcp_temp += mcp.temperature
+
+                bme_gas += bme.gas
+                bme_hum += bme.humidity
+                bme_press += bme.pressure
+                bme_temp += bme.temperature
+
+                try:
+                    pmdata = pm25.read()
+                    pm1_env += pmdata["pm10 env"]
+                    pm25_env += pmdata["pm25 env"]
+                    pm10_env += pmdata["pm100 env"]
+
+                    pm1_st += pmdata["pm10 standard"]
+                    pm25_st += pmdata["pm25 standard"]
+                    pm10_st += pmdata["pm100 standard"]
+                
+                    pm_avg += 1
+                except:
+                    Exception
+
+                try:
+                    if scd.data_available == 1:
+                        scd_co2 += scd.CO2
+                        scd_hum += scd.relative_humidity
+                        scd_temp += scd.temperature
+                        scd_avg += 1
+                except:
+                    Exception
 
                 time.sleep(wait_time)
-                
+            
                 i += 1
         
             avg_purple_air_current = purple_air_current/avg
@@ -366,17 +447,43 @@ def file_write(tca, wait_time, n_points, n_tests, avg):
             avg_comms_power = comms_power/avg
             avg_comms_voltage = comms_voltage/avg
 
-            avg_temp_temp = temp_temp/avg
+            avg_fans_current = fans_current/avg
+            avg_fans_power = fans_power/avg
+            avg_fans_voltage = fans_voltage/avg
+        
+            avg_mcp_temp = mcp_temp/avg
+
+            avg_bme_gas = bme_gas/avg
+            avg_bme_hum = bme_hum/avg
+            avg_bme_press = bme_press/avg
+            avg_bme_temp = bme_temp/avg
+
+            avg_pm1_env = pm1_env/pm_avg 
+            avg_pm25_env = pm25_env/pm_avg
+            avg_pm10_env = pm10_env/pm_avg
+
+            avg_pm1_st = pm1_st/pm_avg
+            avg_pm25_st = pm25_st/pm_avg
+            avg_pm10_st = pm10_env/pm_avg
+
+            avg_scd_co2 = scd_co2/scd_avg
+            avg_scd_hum = scd_hum/scd_avg
+            avg_scd_temp = scd_temp/scd_avg
+
             end_time = datetime.now().strftime('%m/%d/%Y %H:%M:%S')
 
                
             row = [start_time, avg_purple_air_current, avg_purple_air_power, avg_purple_air_voltage,
-                avg_wifi_current, avg_wifi_power, avg_wifi_voltage,
-                avg_raspberry_pi_current, avg_raspberry_pi_power, avg_raspberry_pi_voltage,
-                avg_comms_current, avg_comms_power, avg_comms_voltage, avg_temp_temp]
+                    avg_wifi_current, avg_wifi_power, avg_wifi_voltage,
+                    avg_raspberry_pi_current, avg_raspberry_pi_power, avg_raspberry_pi_voltage,
+                    avg_comms_current, avg_comms_power, avg_comms_voltage, avg_fans_current, 
+                    avg_fans_power, avg_fans_voltage, avg_mcp_temp, avg_bme_temp,
+                    avg_scd_temp, avg_bme_hum, avg_scd_hum, avg_bme_gas, avg_scd_co2, avg_bme_press,
+                    avg_pm1_env, avg_pm25_env, avg_pm10_env, avg_pm1_st, avg_pm25_env, avg_pm10_st]
             
             print(file_name)
             print(row, start_time, end_time)
+            print(i, scd_avg, pm_avg)
             file_writer.writerow(row)
             
             j += 1
@@ -394,7 +501,7 @@ if __name__ == '__main__':
     parser.add_argument("-t", "--n_tests", type=int,  help = "Number of tests to run, break up n_points into n_test chunks. Ex.) --n_tests = 3. --n_points = 60, --wait_time = 1 will give 3 separate tests of data points every second for 60 seconds. If no parameter is give, an indefinite number of tests will be run. ", default=-1)
     parser.add_argument("-c", "--channels", type=list, help="List of channels to enable on mux board, default=all, Ex.) [0, 3] enables channels 0 and 3", default=[0, 1, 2, 3, 4, 5, 6, 7])
     parser.add_argument("-test", "--testing", type=str, help="Is this run for testing or for true data collection? Yes or No?", default="No")
-    parser.add_argument("-a", "--average", type=int, help="The number of points to average over", default=1)
+    parser.add_argument("-a", "--average", type=int, help="The number of points to average over, should default to n_points", default=1)
     args = parser.parse_args()
 
     try:
